fe5786fa2aec75de701bb3fe66271820080ba6b0
Alvien01 <alfinardi543@gmail.com>
2025-11-13 17:16:52 +0700

    migration

---
+++ database/migrations/2025_11_13_100300_create_dosen_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('dosens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('prodi_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->integer('nip')->unique();
+            $table->string('alamat');
+            $table->enum('gender', ['L','P']);
+            $table->string('religion');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('dosens');
+    }
+};
