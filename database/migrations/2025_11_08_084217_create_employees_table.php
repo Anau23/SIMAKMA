e793f3cef597cfaf7a109292e903d5622973f652
Alvien01 <alfinardi543@gmail.com>
2025-11-13 14:49:50 +0700

    first commit

---
+++ database/migrations/2025_11_08_084217_create_employees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('employee_number')->unique();
+            $table->string('position')->nullable();
+            $table->string('department')->nullable();
+            $table->date('join_date')->nullable();
+            $table->decimal('salary', 15, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
